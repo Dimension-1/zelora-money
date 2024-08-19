d39b7ec039764c578e40e1d029dea2374398041a
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2024-08-20 00:57:44 +0530

    ui changes

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -21,7 +21,7 @@ const cardsData = [
 
 const CardsSection = () => {
     return (
-        <section className="py-12">
+        <section className="py-12 mx-8">
             <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {cardsData.map(card => (
                     <div key={card.id} className="bg-[#EBEBEB] p-6 rounded-lg shadow-lg flex flex-col items-center">
--- src/components/HeaderAndHeroSection.jsx
+++ src/components/HeaderAndHeroSection.jsx
@@ -5,20 +5,21 @@ const HeaderAndHeroSection = () => {
     return (
         <section
             className="bg-cover bg-center text-white"
-            style={{ backgroundImage: `url('/Images/Navbar/Bg.png')` }} // Use background image for the entire section
+            style={{ backgroundImage: `url('/Images/Navbar/Bg.png')` }} 
         >
             {/* Navbar */}
             <nav className="flex justify-between items-center p-6 bg-transparent absolute top-0 w-full">
                 <div className="text-red-600 text-4xl font-bold">
-                    <img src="/Images/Navbar/Logo.png" alt="Logo" className="h-10 w-10" />
+                    <img src="/Images/Navbar/Logo.png" alt="Logo" className="h-18 w-18" />
                 </div>
                 <ul className="flex space-x-8 text-white text-lg">
                     <li><a href="#" className="hover:underline">Courses</a></li>
                     <li><a href="#" className="hover:underline">Trade Rooms</a></li>
                     <li><a href="#" className="hover:underline">Community</a></li>
                     <li><a href="#" className="hover:underline">Resources</a></li>
+                    <button className="bg-blue-600 text-white py-2 px-4 rounded-full">Become a citizen</button>
                 </ul>
-                <button className="bg-blue-600 text-white py-2 px-4 rounded-full">Become a citizen</button>
+                
             </nav>
 
             {/* Hero Section */}
